d2752884590c2cefa81b77b873ef39e257af8709
yanzijun <c75a90@gmail.com>
2019-12-19 15:28:32 +0800

    Update theme settings

--- dev/index.js
+++ dev/index.js
@@ -7,7 +7,21 @@ import router from './router'
 
 Vue.config.performance = true
 
-Vue.use(Vuetify)
+Vue.use(
+  Vuetify,
+  {
+    theme:{
+      primary: '#2096f3',
+      secondary: '#ff5722',
+      danger: '#dd2c00',
+      success: '#7ed321',
+      teal: '#00bfa5',
+      'czn-blue': '#147bd1',
+      'light-gray': '#d4d4d4',
+      'dark-gray': '1b2733'
+    }
+  }
+)
 Vue.use(VueRouter)
 
 Vue.component(Boilerplate.name, Boilerplate)
